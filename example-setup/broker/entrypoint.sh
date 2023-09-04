eb763bca272d5d1675cf8d9d80da8107f8545de5
edabrows <95472556+brovoca@users.noreply.github.com>
2023-09-04 12:57:13 +0200

    Cat the gerrit.config for debugging purposes

--- example-setup/broker/entrypoint.sh
+++ example-setup/broker/entrypoint.sh
@@ -4,13 +4,13 @@ function setup_replication_config {
 
   echo "Replacing variables for file /var/gerrit/etc/replication.config.template"
   cat /var/gerrit/etc/replication.config.template | envsubst | sed 's/#{name}#/${name}/g' > /var/gerrit/etc/replication.config
-
   cat /var/gerrit/etc/replication.config
 }
 
 function setup_gerrit_config {
   echo "Replacing variables for file /var/gerrit/etc/gerrit.config.template"
   cat /var/gerrit/etc/gerrit.config.template | envsubst > /var/gerrit/etc/gerrit.config
+  cat /var/gerrit/etc/gerrit.config
 }
 
 JAVA_OPTS='--add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.lang.invoke=ALL-UNNAMED'
